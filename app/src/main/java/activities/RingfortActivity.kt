706627c3a93f9662103e4e81c9187cae72cd13da
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-10-28 15:47:22 +0000

    Image now stored & viewed when editing ringfort

--- app/src/main/java/activities/RingfortActivity.kt
+++ app/src/main/java/activities/RingfortActivity.kt
@@ -1,10 +1,14 @@
 package activities
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import helpers.readImage
+import helpers.readImageFromPath
+import helpers.showImagePicker
 import kotlinx.android.synthetic.main.activity_ringfort.*
 import main.MainApp
 import org.jetbrains.anko.info
@@ -17,6 +21,7 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
 
     var ringfort = RingfortModel()
     lateinit var app : MainApp
+    val IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +39,7 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
             ringfortTitle.setText(ringfort.title)
             description.setText(ringfort.description)
             btnAdd.setText(R.string.save_ringfort)
+            ringfortImage.setImageBitmap(readImageFromPath(this, ringfort.image))
         }
 
         btnAdd.setOnClickListener() {
@@ -52,6 +58,10 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
             setResult(AppCompatActivity.RESULT_OK)
             finish()
         }
+
+        chooseImage.setOnClickListener {
+            showImagePicker(this, IMAGE_REQUEST)
+        }
     }
 
 
@@ -67,5 +77,17 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    ringfort.image = data.getData().toString()
+                    ringfortImage.setImageBitmap(readImage(this, resultCode, data))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/models/RingfortModel.kt
+++ app/src/main/java/models/RingfortModel.kt
@@ -5,5 +5,6 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class RingfortModel(var id: Long = 0,
+                         var image: String = "",
                          var title: String = "",
                           var description: String = "") : Parcelable
