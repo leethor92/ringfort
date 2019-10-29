94629742bea3c216a0fa7bec4d9331e40f0a3f5c
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-10-29 20:26:32 +0000

    Splash screen implemented & app logo created

--- app/src/main/java/main/MainApp.kt
+++ app/src/main/java/main/MainApp.kt
@@ -6,12 +6,14 @@ import models.RingfortMemStore
 import models.RingfortStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.ringfort.R
 
 class MainApp : Application(), AnkoLogger {
 
     lateinit var ringforts: RingfortStore
 
     override fun onCreate() {
+        setTheme(R.style.AppTheme)
         super.onCreate()
         ringforts = RingfortJSONStore(applicationContext)
         info("Ringfort started")
